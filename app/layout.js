ae20509f606d41e344e8557aadf56a3bef226115
joelmce <joelanthony.mac@gmail.com>
2023-10-12 19:28:33 +1100

    Fix context

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import NavBar from "@/components/navigation/NavBar";
-import { UserContext } from '@/context/UserContext'
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Session from "@/context/UserContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="text-white">
-        <UserContext.Provider value={""}>
+        <Session>
           <NavBar />
           <main className="m-4 select-none">{children}</main>
-        </UserContext.Provider>
+        </Session>
       </body>
     </html>
   );
