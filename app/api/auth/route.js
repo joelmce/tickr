e290567c8dd68b396e5ea3ca4d4653251d7aa45a
Joel McElroy <joelanthony.mac@gmail.com>
2023-10-13 00:41:20 +1100

    Fix callback & remove prisma
    Prisma is now redundant considering supabase is going to be doing a lot of our heavylifting

--- app/api/auth/route.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from '@/supabase/supabaseClient'
-import bcrypt from 'bcrypt'
-
-export async function POST(req) {
-    const { email, password } = await req.json()
-    return Response.json({ response: "Successful", status: 200})
-}
--- components/auth/SignUp.js
+++ components/auth/SignUp.js
@@ -1,46 +1,40 @@
-'use client'
+"use client";
 
-import { supabase } from "@/supabase/supabaseClient"
-import { useState } from "react"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function SignUp() {
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const router = useRouter();
+  const supabase = createClientComponentClient();
 
-    const updateEmail = (e) => {
-        setEmail(e.target.value)
-    }
+  const updateEmail = (e) => setEmail(e.target.value);
 
-    const updatePassword = (e) => {
-        setPassword(e.target.value)
-    }
+  const updatePassword = (e) => setPassword(e.target.value);
 
-    const handleSignUp = async (e) => {
-        e.preventDefault();
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email: "joelanthony.mac@gmail.com",
+      password: "testing",
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+    router.refresh();
+  };
 
-        const {data, error} = await supabase.auth.signUp({
-            email,
-            password
-        })
-
-        console.log(data, error)
-
-        console.log('Submitted:', {email, password})
-    }
-
-
-
-    return (
-        <form>
-            <label>
-                Email
-                <input type="email" onChange={updateEmail}/>
-            </label>
-            <label>
-                Password
-                <input type="password" onChange={updatePassword}/>
-            </label>
-            <button onClick={handleSignUp}>Sign Up</button>
-        </form>
-    )
+  return (
+    <>
+      <label>
+        Email
+        <input type="email" onChange={updateEmail} />
+      </label>
+      <label>
+        Password
+        <input type="password" onChange={updatePassword} />
+      </label>
+      <button onClick={handleSignUp}>Sign Up</button>
+    </>
+  );
 }
