5df254dfd3e2d3378b777875d71bd6b9e266c7e4
Nikita O'Keeffe <snow@Nikitas-MacBook-Air.local>
2023-10-14 17:37:54 +1100

    UI fixes (navbar, dashboard)

--- app/(dashboard)/[dashboard]/page.js
+++ app/(dashboard)/[dashboard]/page.js
@@ -1,14 +1,11 @@
-import Dashboard from "/components/dashboard/Dashboard.js"
+import Dashboard from "/components/dashboard/Dashboard.js";
 
 async function getDashboard(id) {}
 
 export default async function Page({ params }) {
-
   return (
     <>
-      <h1 className="text-3xl">Dashboard</h1>
-      <Dashboard/>
-      
+      <Dashboard />
     </>
   );
 }
