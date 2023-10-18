9f435e269c0b4b96dda56421254c29f48c967dc1
joelmce <joelanthony.mac@gmail.com>
2023-10-19 00:29:33 +1100

    Multi step form

--- app/(dashboard)/dashboards/new/page.js
+++ app/(dashboard)/dashboards/new/page.js
@@ -1,4 +1,5 @@
 import NewDashboardForm from "@/components/dashboard/NewDashboardForm";
+import MultiStepForm from "@/components/forms/MultiStepForm";
 
 export default function Page() {
     return <NewDashboardForm/>
---
+++ components/forms/MultiStepForm.js
@@ -0,0 +1,69 @@
+'use client'
+import { useState } from "react";
+
+export function Step1({ nextStep }) {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        nextStep()
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Step 1</h2>
+            <button type="submit">Next</button>
+        </form>
+    )
+}
+
+export function Step2({ prevStep, nextStep }) {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        nextStep()
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Step 2</h2>
+            <button onClick={prevStep}>Previous</button>
+            <button type="submit">Next</button>
+        </form>
+    )
+}
+
+export function Step3({ prevStep }) {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Step 3</h2>
+            <button onClick={prevStep}>Previous</button>
+            <button type="submit">Submit</button>
+        </form>
+    )
+} 
+
+export default function MultiStepForm() {
+    const [step, setStep] = useState(1)
+    console.log(step)
+
+    const nextStep = () => {
+        setStep(step + 1)
+    }
+
+    const prevStep = () => {
+        setStep(step - 1)
+    }
+
+    switch (step) {
+        case 1:
+          return <Step1 nextStep={nextStep} />;
+        case 2:
+          return <Step2 prevStep={prevStep} nextStep={nextStep} />;
+        case 3:
+          return <Step3 prevStep={prevStep} />;
+        default:
+          return null;
+      }
+}
