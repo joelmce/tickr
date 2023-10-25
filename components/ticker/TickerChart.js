c2ef4673462eea31e65357a853d02687a4f05ba0
Joel McElroy <joelanthony.mac@gmail.com>
2023-10-26 10:03:34 +1100

    Add live data fetching to ticker graphs

--- components/ticker/TickerChart.js
+++ components/ticker/TickerChart.js
@@ -1,13 +1,41 @@
 "use client";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
+import { useState } from "react";
 
-export default function TickerChart({ data }) {
+export default function TickerChart({ ticker }) {
+  const [historicalData, setHistoricalData] = useState([])
+
+  const fetchData = async() => {
+    try {
+      const interval = "1hr"
+
+      const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${ticker}&interval=${interval}`);
+      const data = await response.json()
+
+      setHistoricalData(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+
+    const updateInterval = setInterval(() => {
+       fetchData() 
+    }, 60 * 60 * 1000) // 60 minutes
+
+    return () => {
+      clearInterval(updateInterval)
+    }
+  }, [])
+  
   const chartData = {
-    labels: data.map((item) => item.date),
+    labels: historicalData.map((candle) => new Date(candle[0]).toLocaleTimeString()),
     datasets: [
       {
-        data: data.map((item) => item.price),
+        data: historicalData.map((candle) => parseFloat((candle[4])),
         borderColor: "rgba(48, 190, 42, 0.8)",
         backgroundColor: "rgba(12, 48, 10, 0.2)", // Dyamamic
         lineTension: 0.1,
