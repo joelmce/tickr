6883c578521d374c43d6d8dac5fbe058f3bcf1bd
Joel McElroy <joelanthony.mac@gmail.com>
2023-10-28 10:17:34 +1100

    fix delete button colour

--- components/ui/DeleteConfirmationButton.js
+++ components/ui/DeleteConfirmationButton.js
@@ -1,6 +1,12 @@
-'use client'
+"use client";
 import { DeleteForever, WarningRounded } from "@mui/icons-material";
-import { DialogActions, DialogContent, Divider, Modal, ModalDialog } from "@mui/joy";
+import {
+  DialogActions,
+  DialogContent,
+  Divider,
+  Modal,
+  ModalDialog,
+} from "@mui/joy";
 import Button from "@mui/joy/Button";
 import { DialogTitle } from "@mui/material";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
@@ -10,54 +16,61 @@ import { CircularProgress } from "@mui/joy";
 import { Alert } from "./Alert";
 
 export function DeleteConfirmationButton({ name, dashboard_id }) {
-    const [open, setOpen] = useState(false);
-    const [loading, isLoading] = useState(false)
-    const router = useRouter()
+  const [open, setOpen] = useState(false);
+  const [loading, isLoading] = useState(false);
+  const router = useRouter();
 
-    const handleDelete = async() => {
-        isLoading(true)
-        setOpen(false)
-        const supabase = createClientComponentClient()
-        const { error } = await supabase
-            .from("Dashboards")
-            .delete()
-            .eq("dashboard_id", dashboard_id);
-            if (error) console.log(error);
-        isLoading(false)
+  const handleDelete = async () => {
+    isLoading(true);
+    setOpen(false);
+    const supabase = createClientComponentClient();
+    const { error } = await supabase
+      .from("Dashboards")
+      .delete()
+      .eq("dashboard_id", dashboard_id);
+    if (error) console.log(error);
+    isLoading(false);
 
-        
-        router.refresh();
-    }
+    router.refresh();
+  };
 
-    return (
-        <>
+  return (
+    <>
+      <Button
+        variant="outlined"
+        color="danger"
+        endDecorator={<DeleteForever />}
+        onClick={() => setOpen(true)}
+      >
+        Delete
+      </Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalDialog variant="outlined" role="alertdialog">
+          <DialogTitle>
+            <WarningRounded />
+            Confirmation
+          </DialogTitle>
+          <Divider />
+          <DialogContent>Are you sure you want to delete {name}?</DialogContent>
+          <DialogActions>
             <Button
-            variant="outlined"
-            color="danger"
-            endDecorator={<DeleteForever/>}
-            onClick={() => setOpen(true)}>
-                Delete
+              variant="solid"
+              className="bg-red-700 hover:bg-red-800"
+              onClick={handleDelete}
+              disabled={loading ? true : false}
+            >
+              {loading ? <CircularProgress /> : "Delete"}
             </Button>
-            <Modal open={open} onClose={() => setOpen(false)}>
-                <ModalDialog variant="outlined" role="alertdialog">
-                    <DialogTitle>
-                        <WarningRounded/>
-                        Confirmation
-                    </DialogTitle>
-                    <Divider/>
-                    <DialogContent>
-                        Are you sure you want to delete {name}?
-                    </DialogContent>
-                    <DialogActions>
-                        <Button variant="solid" color="danger" onClick={handleDelete} disabled={loading ? true : false}>
-                            {loading ? <CircularProgress/> : "Delete"}
-                        </Button>
-                        <Button variant="plain" color="neutral" onClick={() => setOpen(false)}>
-                            Cancel
-                        </Button>
-                    </DialogActions>
-                </ModalDialog>
-            </Modal>
-        </>
-    )
+            <Button
+              variant="plain"
+              color="neutral"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </Button>
+          </DialogActions>
+        </ModalDialog>
+      </Modal>
+    </>
+  );
 }
